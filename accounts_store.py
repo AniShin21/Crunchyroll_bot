67ea017c91158801a799c5b1ebc1666b9b8adae8
AniShin <minecraft2727k@gmail.com>
2024-10-28 19:49:03 +0530

    Update accounts_store.py

--- accounts_store.py
+++ accounts_store.py
@@ -9,13 +9,24 @@ user_data = {}
 
 
 
+async def add_user(user_id):
+    """Add a user to user_data if not already present."""
+    if user_id not in user_data:
+        # Initialize user data
+        user_data[user_id] = {
+            "first_name": None,
+            "last_name": None,
+            "username": None,
+            "other_data": {}  # Add any other fields you want to track
+        }
+
 
 
 # List of banned user IDs
 banned_users = []
 
 # Message to display for banned users
-BANNED_MESSAGE = "🚫 You are banned from using this bot."
+BANNED_MESSAGE = "❌ 𝖸𝗈𝗎 𝖺𝗋𝖾 𝖻𝖺𝗇𝗇𝖾𝖽 𝖿𝗋𝗈𝗆 𝗎𝗌𝗂𝗇𝗀 𝗍𝗁𝗂𝗌 𝖻𝗈𝗍."
 
 # Filter to allow only unbanned users
 unban = filters.create(lambda _, __, message: message.from_user.id not in banned_users)
